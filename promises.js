9094403c644861c47ea4587335f35ad6a54a857e
Stephen2702 <stephenemmanuel27072002@outlook.com>
2023-08-21 10:53:22 +0530

    async await

--- promises.js
+++ promises.js
@@ -1,66 +1,41 @@
-const posts = [
-    { title: 'Post One', body: 'This is post one' },
-    { title: 'Post Two', body: 'This is post two' }
-];
+console.log('person1 shows ticket');
+console.log('person2 shows ticket');
 
-function getPosts() {
-    setTimeout(() => {
-        let output = '';
-        posts.forEach((post, index) => {
-            output += `<li>${post.title}</li>`;
-        });
-        document.body.innerHTML = output;
-    }, 1000);
-}
+const getColdDrinks = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('cold drinks'), 2000);
+});
 
-function createPost(post) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            posts.push(post);
-            const error = false;
-
-            if (!error) {
-                resolve();
-            } else {
-                reject('Error: something is wrong');
-            }
-        }, 2000);
+const preMovie = async () => {
+    const person3PromiseToShowTicketWhenWifeArrives = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('ticket'), 3000);
     });
-}
 
-function updateLastUserActivityTime() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            const lastActivityTime = new Date().toLocaleTimeString();
-            console.log(`Last Activity Time: ${lastActivityTime}`);
-            resolve(lastActivityTime);
-        }, 1000);
+    const getPopcorn = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('popcorn'), 3000);
     });
-}
 
-createPost({ title: 'Post three', body: 'this is post three' })
-    .then(updateLastUserActivityTime)
-    .then((lastActivityTime) => {
-        getPosts();
-        console.log(`Posts created at: ${lastActivityTime}`);
-        return deleteLastPost();
-    })
-    .then(() => {
-        console.log('Post deleted successfully');
-        getPosts();
-    })
-    .catch((err) => console.log(err));
-
-function deleteLastPost() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (posts.length > 0) {
-                const deletedPost = posts.pop();
-                console.log(`Deleted Post: ${deletedPost.title}`);
-                resolve();
-            } else {
-                reject('No posts to delete');
-            }
-        }, 1000);
+    const getCandy = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('candy'), 3000);
     });
-}
+
+    const getCoke = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('coke'), 3000);
+    });
+
+    let ticket = await person3PromiseToShowTicketWhenWifeArrives;
+
+    let [popcorn, candy, coke] = await Promise.all([getPopcorn, getCandy, getCoke]);
+
+    console.log(`got ${popcorn}, ${candy}, ${coke}`);
+
+    const coldDrinks = await getColdDrinks;
+
+    return { ticket, coldDrinks };
+};
+
+preMovie().then(({ ticket, coldDrinks }) => {
+    console.log(`person4 shows ${ticket}`);
+    console.log(`person4 shows ${coldDrinks}`);
+});
+
+console.log('person4 shows ticket');
